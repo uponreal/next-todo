51daf0a803b9dba497376269f1113addfcd4f36c
Uponreal <uponreal@gmail.com>
2021-02-22 01:47:34 +0900

    TodoList 컴포넌트 추가

---
+++ components/TodoList.js
@@ -0,0 +1,14 @@
+export default () => {
+    return (
+        <>
+            <div className="container">
+                <h1>Todo List</h1>
+            </div>
+            <style jsx>{`
+                .container {
+                    width: 100%;
+                }       
+            `}</style>
+        </>
+    )
+}
