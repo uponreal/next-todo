c4b5e59000d2e8eac7f40bc76f2165a08ab3e0c9
Uponreal <uponreal@gmail.com>
2021-02-22 01:50:42 +0900

    Dummy Todo data 추가, 남은 Todo 수량 표시

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,13 +1,29 @@
-export default () => {
+import palette from "../theme/palette";
+
+export default ({todos}) => {
     return (
         <>
             <div className="container">
-                <h1>Todo List</h1>
+                <div className="todo-list-header">
+                    <p className="todo-list-last-todo">
+                        남은 TODO<span>{todos.length}개</span>
+                    </p>
+                </div>
             </div>
             <style jsx>{`
                 .container {
                     width: 100%;
-                }       
+                }
+                .container .todo-list-header {
+                    padding: 12px;
+                    border-bottom: 1px solid ${palette.gray}
+                }
+                .container .todo-list-header .todo-list-last-todo {
+                    font-size: 14px;
+                }   
+                .container .todo-list-header .todo-list-last-todo span {
+                    margin-left: 8px;
+                }  
             `}</style>
         </>
     )
