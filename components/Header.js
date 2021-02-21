5cfc8b43891270c9f11f77095c8c131fdb801ed8
Uponreal <uponreal@gmail.com>
2021-02-22 01:45:41 +0900

    Header 컴포넌트 추가 및 사용하지 않는 파일 제거

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import palette from "../theme/palette";
+
+export default () => {
+    return (
+        <>
+            <div className="container">
+                <h1>Todo List</h1>
+            </div>
+            <style jsx>{`
+                .container {
+                    display: flex;
+                    align-items: center;
+                    width: 100%;
+                    height: 52px;
+                    padding: 0 12px;
+                    border-bottom: 1px solid ${palette.gray};
+                }
+                .container h1 {
+                    font-size: 21px;
+                }
+            `}</style>
+        </>
+    )
+}
