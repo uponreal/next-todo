4ae0f298aeb53fdcf551a1da57bb200b2303299f
Uponreal <uponreal@gmail.com>
2021-02-24 10:57:44 +0900

    TodoList 숫자별 남은 수량 표시
    TodoList 숫자별 남은 수량 표시

--- components/TodoList.js
+++ components/TodoList.js
@@ -20,26 +20,67 @@ const TodoList = ({todos}) => {
     return (
         <>
             <div className="container">
-                <div className="todo-list-header">
-                    <p className="todo-list-last-todo">
+                <div className="header">
+                    <p className="header__last-todo">
                         남은 TODO<span>{todos.length}개</span>
                     </p>
+                    <div className="header__colors">
+                        {Object.keys(todoColorCount).map((color, index)=>{
+                            return (
+                                <div className="header__color" key={index}>
+                                    <div className={`header__round-color bg-${color}`}></div>
+                                    <p>{todoColorCount[color]}개</p>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
             <style jsx>{`
                 .container {
                     width: 100%;
                 }
-                .container .todo-list-header {
+                .header {
                     padding: 12px;
                     border-bottom: 1px solid ${palette.gray}
                 }
-                .container .todo-list-header .todo-list-last-todo {
+                .header__last-todo {
                     font-size: 14px;
-                }   
-                .container .todo-list-header .todo-list-last-todo span {
+                    margin-bottom: 4px;
+                }
+                .header__last-todo span {
                     margin-left: 8px;
-                }  
+                }
+                .header__colors {
+                    display: flex;
+                }
+                .header__color {
+                    display: flex;
+                    margin-right: 8px;
+                }
+                .header__round-color {
+                    width: 16px;
+                    height: 16px;
+                    border-radius: 50%;
+                }
+                .bg-blue {
+                    background-color: ${palette.blue};
+                }
+                .bg-green {
+                    background-color: ${palette.green};
+                }
+                .bg-navy {
+                    background-color: ${palette.navy};
+                }
+                .bg-orange {
+                    background-color: ${palette.orange};
+                }
+                .bg-red {
+                    background-color: ${palette.red};
+                }
+                .bg-yellow {
+                    background-color: ${palette.yellow};
+                }
             `}</style>
         </>
     );
