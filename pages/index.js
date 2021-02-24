02c12fecab04227c26977c244e425e332adfd3f5
Uponreal <uponreal@gmail.com>
2021-02-24 18:04:17 +0900

    todos api 사용하여 todo 데이터 가져오기 - 클라이언트에 SWR 함께 적용

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import TodoList from "../components/TodoList.js";
-import axios from "axios";
+import { getTodos } from "../lib/api/todo";
+import useSWR from "swr";
 
-export async function getServerSideProps(context) {
-  const result = await axios.get("http://localhost:3000/api/todos");
-  console.log(result.data);
+export async function getServerSideProps() {
+  const result = await getTodos();
   return {
     props: {
       todos: result.data,
@@ -12,6 +12,14 @@ export async function getServerSideProps(context) {
 }
 
 const App = ({ todos }) => {
+  const { data, error } = useSWR("getTodos", async () => {
+    const result = await getTodos();
+    return result.data;
+  });
+  if (error) return <div>failed to load</div>;
+  if (data !== undefined) {
+    todos = data;
+  }
   return <TodoList todos={todos} />;
 };
 
