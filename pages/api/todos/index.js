bbeada7330ccea974d4880729e0c177527b84d19
Uponreal <uponreal@gmail.com>
2021-02-24 19:13:55 +0900

    todo 데이터 가져오는 부분 lib 분리

---
+++ pages/api/todos/index.js
@@ -0,0 +1,17 @@
+import data from "../../../lib/data/index";
+
+export default (req, res) => {
+  if (req.method === "GET") {
+    try {
+      const todos = data.todo.getTodos();
+      return res.status(200).send(todos);
+    } catch (e) {
+      console.log(e);
+      res.statusCode = 500;
+      return res.send(e);
+    }
+  } else {
+    res.statusCode = 405;
+    return res.end();
+  }
+};
